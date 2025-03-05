e8b04d41f12dfad927fc2aaec50770d6da3e0f5f
thaihoc1310 <thaihoc131005@gmail.com>
2025-03-06 00:42:52 +0700

    login with oauth2.0

---
+++ src/main/java/com/thaihoc/miniinsta/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.thaihoc.miniinsta.repository;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.thaihoc.miniinsta.model.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    // @EntityGraph(attributePaths = "authorities")
+    Optional<User> findByUsername(String username);
+}
